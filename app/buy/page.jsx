e2101b6cbacc1f1e95c261a843bedb952937deca
afterrender1 <afterrender1@gmail.com>
2025-12-03 18:18:58 +0500

    reviews component added

--- app/buy/page.jsx
+++ app/buy/page.jsx
@@ -1,6 +1,7 @@
 import AboutTmg from '@/components/AboutTmg'
 import BuyHome from '@/components/BuyHome'
 import BuyOurLatestArrivals from '@/components/BuyOurLatestArrivals'
+import Reviews from '@/components/Reviews'
 import TmgBehindScenesVideo from '@/components/TmgBehindScenesVideo'
 import TmgFinanceAndVan from '@/components/TmgFinanceAndVan'
 import WorkingInPartnership from '@/components/WorkingInPartnership'
@@ -15,6 +16,8 @@ const page = () => {
     <TmgFinanceAndVan/>
     <TmgBehindScenesVideo/>
     <WorkingInPartnership/>
+    <Reviews/>
+    
     
     </>
   )
